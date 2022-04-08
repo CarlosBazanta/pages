201bc3b167110e1737bd5b167e42535f0aae6905
Carlos Gonzalez <carlos.gonzalez@tcs.local>
2022-04-08 13:41:04 -0400

    se implemento lazyload, se incluyo una imagen el fondo y se cambio el color del titulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './core/pages/inicio/inicio.component';
-import { ExamplesComponent } from './core/pages/examples/examples.component';
-import { ProfileComponent } from './core/pages/profile/profile.component';
-import { ContactComponent } from './core/pages/contact/contact.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent,
-    ExamplesComponent,
-    ProfileComponent,
-    ContactComponent
+
   ],
   imports: [
     BrowserModule,

8d40d217f3585d9db17b146c5a297a9afc6d7018
Carlos Gonzalez <carlos.gonzalez@tcs.local>
2022-04-22 13:38:33 -0400

    se cambio el header a app componet y se agregarpn servivicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { HelperService } from './Service/helper.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,21 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'test';
+
+  selectedLanguage = 'en';
+
+  constructor( private translateService: TranslateService, private helper: HelperService) {
+
+        this.translateService.setDefaultLang(this.selectedLanguage);
+        this.translateService.use(this.selectedLanguage);
+
+   }
+
+   toogleLanguage(lang: string) {
+    this.translateService.use(lang);
+    this.helper.setLangueage(lang);
+    this.helper.getLanguege().subscribe(lang=>{console.log(lang)})
+
+}
+
 }
